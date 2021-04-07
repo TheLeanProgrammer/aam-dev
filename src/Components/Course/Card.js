5cd324fe937c0d397ea52091e803bf7b3771b31e
smilegupta <guptamiley3012@gmail.com>
2021-04-08 00:35:17 +0530

    warnings fixed for course section

--- src/Components/Course/Card.js
+++ src/Components/Course/Card.js
@@ -4,13 +4,13 @@ import "./Course.css";
 const Card = ({ cardTitle, cardContent, cardImage, cardUrl }) => {
   return (
     <div className="col-md-3 col-lg-3 col-xs-12 mb-4">
-      <div class="card" style={{ borderRadius: "25px" }}>
-        <img class="card-img-top" src={cardImage} alt={cardTitle} />
-        <div class="card-body">
-          <h5 class="card-title">{cardTitle}</h5>
-          <p class="card-text">{cardContent}</p>
-          <a href={cardUrl} class="see-more" target="_blank" rel="noreferrer">
-            Go somewhere -&gt;
+      <div className="card" style={{ borderRadius: "25px" }}>
+        <img className="card-img-top" src={cardImage} alt={cardTitle} />
+        <div className="card-body">
+          <h5 className="card-title">{cardTitle}</h5>
+          <p className="card-text">{cardContent}</p>
+          <a href={cardUrl} className="see-more" target="_blank" rel="noreferrer">
+            Watch More -&gt;
           </a>
         </div>
       </div>
--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -7,19 +7,20 @@ const Course = () => {
     <div>
       <div className="row px-5 mb-4">
         <div className="col-12 text-center">
-        <div class="second-headline pb-3">
+        <div className="second-headline pb-3">
               <h2>What will we learn in this course?</h2>
             </div>
           
         </div>
       </div>
       <div className="row px-5">
-        {courses.map((item) => (
+        {courses.map((item,idx) => (
           <Card
             cardTitle={item.cardTitle}
             cardContent={item.cardContent}
             cardImage={item.cardImage}
             cardUrl={item.cardUrl}
+            key={idx}
           />
         ))}
       </div>
